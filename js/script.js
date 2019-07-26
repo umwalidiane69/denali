fe8a635fd0425d9688fb44c14ff33599c02d2713
umwalidiane <umwalidiane69@gmail.com>
2019-07-26 19:00:33 +0200

    hidding first paragraph

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".dor").click(function(){
+        $("#im").toggle();
+        $("#pa").toggle();
+    });
+    $(".doru").click(function(){
+        $("#am").toggle();
+        $("#par").toggle();
+    });
+    $(".koe").click(function(){
+        $("#las").toggle();
+        $("#the").toggle();
+    });
+});
