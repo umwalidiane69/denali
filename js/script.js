be28e60286cd6072531f1928a3820cfc591db09e
umwalidiane <umwalidiane69@gmail.com>
2019-07-29 17:06:03 +0200

    adding jquery

--- js/script.js
+++ js/script.js
@@ -18,68 +18,68 @@ $(document).ready(function(){
     function(){
             $("#firt").css("opacity","1");
             $("#va").hide();  }  
-// )
-//     $("#im3").hover(function() {
-//         $("#thr").css("opacity","0.5");
-//         $("#ve").show();
-//     },
-//     function(){
-//             $("#thr").css("opacity","1");
-//             $("#ve").hide();  }  
-// )
+ )
+    $("#im3").hover(function() {
+        $("#thr").css("opacity","0.5");
+        $("#ve").show();
+    },
+    function(){
+            $("#thr").css("opacity","1");
+            $("#ve").hide();  }  
+)
 
-// $("#im2").hover(function() {
-//     $("#secn").css("opacity","0.5");
-//     $("#vi").show();
-// },
-// function(){
-//         $("#secn").css("opacity","1");
-//         $("#vi").hide();  }  
-// )
+$("#im2").hover(function() {
+    $("#secn").css("opacity","0.5");
+    $("#vi").show();
+},
+function(){
+        $("#secn").css("opacity","1");
+        $("#vi").hide();  }  
+)
 
-// $("#im4").hover(function() {
-//     $("#fort").css("opacity","0.5");
-//     $("#vu").show();
-// },
-// function(){
-//         $("#fort").css("opacity","1");
-//         $("#vu").hide();  }  
-// )
+$("#im4").hover(function() {
+    $("#fort").css("opacity","0.5");
+    $("#vu").show();
+},
+function(){
+        $("#fort").css("opacity","1");
+        $("#vu").hide();  }  
+)
 
-// $("#im5").hover(function() {
-//     $("#fith").css("opacity","0.5");
-//     $("#hi").show();
-// },
-// function(){
-//         $("#fith").css("opacity","1");
-//         $("#hi").hide();  }  
-// )
+$("#im5").hover(function() {
+    $("#fith").css("opacity","0.5");
+    $("#hi").show();
+},
+function(){
+        $("#fith").css("opacity","1");
+        $("#hi").hide();  }  
+)
 
-// $("#im6").hover(function() {
-//     $("#sixh").css("opacity","0.5");
-//     $("#TE").show();
-// },
-// function(){
-//         $("#sixh").css("opacity","1");
-//         $("#TE").hide();  }  
-// )
+$("#im6").hover(function() {
+    $("#sixh").css("opacity","0.5");
+    $("#TE").show();
+},
+function(){
+        $("#sixh").css("opacity","1");
+        $("#TE").hide();  }  
+)
 
-// $("#im7").hover(function() {
-//     $("#sevh").css("opacity","0.5");
-//     $("#hu").show();
-// },
-// function(){
-//         $("#sevh").css("opacity","1");
-//         $("#hu").hide();  }  
-// )
+$("#im7").hover(function() {
+    $("#sevh").css("opacity","0.5");
+    $("#hu").show();
+},
+function(){
+        $("#sevh").css("opacity","1");
+        $("#hu").hide();  }  
+)
 
-// $("#im8").hover(function() {
-//     $("#eigh").css("opacity","0.5");
-//     $("#ha").show();
-// },
-// function(){
-//         $("#eigh").css("opacity","1");
-//         $("#ha").hide();  }  
+$("#im8").hover(function() {
+    $("#eigh").css("opacity","0.5");
+    $("#ha").show();
+},
+function(){
+        $("#eigh").css("opacity","1");
+        $("#ha").hide();  }  
 )
 
 
